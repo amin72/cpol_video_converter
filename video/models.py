331afd5376967fd6e22c01961c4dc37449f9fd48
Amin <=>
2022-07-21 12:59:11 +0430

    define Video model

--- video/models.py
+++ video/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    video_original = models.FileField()
+    video_240 = models.FileField(null=True, blank=True)
+    video_360 = models.FileField(null=True, blank=True)
+    convert_time = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.video_original.name
