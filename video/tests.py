85bd818c846245e1c1c4488cdf77b181dbd196fa
Amin <=>
2022-07-21 17:10:55 +0430

    check if ffmpeg exists and converts videos correctly

--- video/tests.py
+++ video/tests.py
@@ -1,3 +1,32 @@
+import os
+import shutil
+import subprocess
 from django.test import TestCase
+from django.conf import settings
+from .models import Video
 
-# Create your tests here.
+
+class VideoTestCase(TestCase):
+    def test_ffmpeg_command_exists(self):
+        self.assertTrue(shutil.which('ffmpeg'), 'ffmpeg does not exist')
+
+        command_list = [
+            'ffmpeg',
+            '-i',
+            'media/test.mp4',
+            '-vf',
+            'scale=-1:240',
+            '-c:v',
+            'libx264',
+            '-c:a',
+            'copy',
+            'media/test_240.mp4',
+        ]
+
+        # run for scale 240
+        self.assertEqual(subprocess.call(command_list), 0)
+        
+        # run for scale 360
+        command_list[4] = 'scale=-1:360'
+        command_list[-1] = 'media/test_360.mp4'
+        self.assertEqual(subprocess.call(command_list), 0)
