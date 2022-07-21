53a51358f7003c548e1fd2caf45cb803223144f4
Amin <=>
2022-07-21 17:39:11 +0430

    change celery auto discover tasks settigns

--- videoconverter/celery.py
+++ videoconverter/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'videoconverter.settings')
@@ -7,9 +8,7 @@ BASE_REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
 
 app = Celery('videoconverter_celery_app')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-app.autodiscover_tasks()
-
 app.conf.broker_url = BASE_REDIS_URL
 app.conf.result_backend = BASE_REDIS_URL
+
+app.autodiscover_tasks(settings.INSTALLED_APPS)
